a7abe4703f136af74681264ddee757ec1c011717
AlexPerse10 <spuknitfire@gmail.com>
2026-03-06 13:54:46 +0100

    ejercicios de stack pilas

---
+++ Colecciones_poo/src/main/java/org/example/aceptaReto_balanceado.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Stack;
+
+public class aceptaReto_balanceado {
+    public static boolean esbalanceado(String expresion) {
+        Stack <Character> pilacaracteres = new Stack<>();
+        for (int i =0; i<expresion.length(); i++){
+            char caracteres = expresion.charAt(i);
+            if (caracteres == '('){
+                pi
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String expresion = "";
+    }
+}
--- Colecciones_poo/src/main/java/org/example/ejercicio3_pilasstack.java
+++ Colecciones_poo/src/main/java/org/example/ejercicio3_pilasstack.java
@@ -3,10 +3,31 @@ package org.example;
 import java.util.Stack;
 
 public class ejercicio3_pilasstack {
+
+    public static boolean esBalanceado(String expresion) {
+        Stack<Character> pilacaracteres = new Stack<>();
+        for (int i =0; i<expresion.length(); i++){
+           char caracter = expresion.charAt(i);
+
+           if (caracter == '('){
+               pilacaracteres.push(caracter);
+
+           } else if (caracter == ')') {
+               if (pilacaracteres.isEmpty()){  // si la pila esta vacia la expresion es incorrecta
+                   return false;
+               }
+               pilacaracteres.pop(); // si no quito un ( de la pisla
+
+           }
+        }
+        return pilacaracteres.isEmpty();
+
+    }
     public static void main(String[] args) {
-        Stack <Character> listaoperacion = new Stack<>();
-        public boolean esbalanceado (String expresion) {
+        String expresion = "((3+5)*2)";
+
+        System.out.println("es balaceado)" +esBalanceado(expresion));
 
         }
     }
-}
+
