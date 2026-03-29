14369f59a0f6149d5608816d8facc2eed83ea301
AlexPerse10 <spuknitfire@gmail.com>
2026-03-29 12:27:53 +0200

    ejercicio recursivi

--- recuperar_practicas/src/main/java/org/example/Recursividad7.java
+++ recuperar_practicas/src/main/java/org/example/Recursividad7.java
@@ -2,28 +2,28 @@ package org.example;
 
 public class Recursividad7 {
         public static void main(String[] args) {
-            int n = 4;
+            int numero = 4;
 
-            int resultado = suma(n);
-            imprimirCadena(n);
+            int resultado = suma(numero);
+            imprimirCadena(numero);
             System.out.println(" = " + resultado);
         }
 
 
-        public static int suma(int n) {
-            if (n == 1) {
+        public static int suma(int numero) {
+            if (numero == 1) {
                 return 1;
             }
-            return n + suma(n - 1);
+            return numero + suma(numero - 1);
         }
 
 
-        public static void imprimirCadena(int n) {
-            if (n == 1) {
+        public static void imprimirCadena(int numero) {
+            if (numero == 1) {
                 System.out.print("1");
             } else {
-                imprimirCadena(n - 1);
-                System.out.print("+" + n);
+                imprimirCadena(numero - 1);
+                System.out.print("+" + numero);
             }
         }
     }
